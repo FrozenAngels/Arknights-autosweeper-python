b68e409d32008498be4ea0be191aa4bcee5cd2bc
Hakurui_Reimu <583663275@qq.com>
2019-12-28 17:25:17 -0500

    Create arknightsweeper.py

---
+++ arknightsweeper.py
@@ -0,0 +1,15 @@
+#Arknights automatic sweeping script.
+#Warning:0/120 consciousness will be remain.
+import os
+import copy
+class clickpoint(object):
+	def __init__(self,x,y):
+		self.x=int(x)
+		self.y=int(y)
+	def click(self):
+		return self.x,self.y
+print('One device adb_based automatic script for Arknights sweeping')
+try:
+	
+except Exception as e:
+	raise e
